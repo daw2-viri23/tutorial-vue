28c8175c4eca26085d764f30fcb8bcf241f5d1a1
daw2-viri23 <virimurillojoseluis@fpllefia.com>
2022-11-15 19:46:53 +0100

    commit 1

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Personas</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <formulario-persona />
+        <tabla-personas :personas="personas" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TablaPersonas from '@/components/TablaPersonas.vue'
+  import FormularioPersona from '@/components/FormularioPersona.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TablaPersonas,
+    FormularioPersona
+  },
+  data() {
+    return {
+      personas: [
+        {
+          id: 1,
+          nombre: 'Jon',
+          apellido: 'Nieve',
+          email: 'jon@email.com',
+        },
+        {
+          id: 2,
+          nombre: 'Tyrion',
+          apellido: 'Lannister',
+          email: 'tyrion@email.com',
+        },
+        {
+          id: 3,
+          nombre: 'Daenerys',
+          apellido: 'Targaryen',
+          email: 'daenerys@email.com',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+</style>
---
+++ src/components/FormularioPersona.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="formulario-persona">
+    <form @submit.prevent="enviarFormulario">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="form-group">
+              <label>Nombre</label>
+              <input v-model="persona.nombre" type="text" class="form-control" />
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="form-group">
+              <label>Apellido</label>
+              <input v-model="persona.apellido" type="text" class="form-control" />
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="form-group">
+              <label>Email</label>
+              <input v-model="persona.email" type="email" class="form-control" />
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="form-group">
+              <button class="btn btn-primary">Añadir persona</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+  name: 'formulario-persona',
+  data() {
+    return {
+      persona: {
+        nombre: '',
+        persona: '',
+        apellido: '',
+      },
+    }
+  },
+  methods: {
+    enviarFormulario() {
+      console.log('Funciona!');
+    },
+  },
+}
+</script>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+</style>
